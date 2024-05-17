651607622351e6f4bad7e3900bd6f057a14bf32a
peknight <JKpeknight@gmail.com>
2024-05-17 18:01:56 +0800

    update

--- jose-core/shared/src/main/scala/com/peknight/jose/jwk/JsonWebKey.scala
+++ jose-core/shared/src/main/scala/com/peknight/jose/jwk/JsonWebKey.scala
@@ -1,7 +1,14 @@
 package com.peknight.jose.jwk
 
+import com.peknight.jose.jwa.JsonWebAlgorithm
+
+/**
+ * https://datatracker.ietf.org/doc/html/rfc7517
+ */
 trait JsonWebKey:
   def keyType: KeyType
   def publicKeyUseType: Option[PublicKeyUseType]
   def keyOperations: Seq[KeyOperationType]
+  def algorithm: JsonWebAlgorithm
+  
 end JsonWebKey
--- jose-core/shared/src/main/scala/com/peknight/jose/jwk/KeyOperationType.scala
+++ jose-core/shared/src/main/scala/com/peknight/jose/jwk/KeyOperationType.scala
@@ -1,12 +1,13 @@
 package com.peknight.jose.jwk
 
-enum KeyOperationType(val entryName: String):
-  case Sign extends KeyOperationType("sign")
-  case Verify extends KeyOperationType("verify")
-  case Encrypt extends KeyOperationType("encrypt")
-  case Decrypt extends KeyOperationType("decrypt")
-  case WrapKey extends KeyOperationType("wrapKey")
-  case UnwrapKey extends KeyOperationType("unwrapKey")
-  case DeriveKey extends KeyOperationType("deriveKey")
-  case DeriveBits extends KeyOperationType("deriveBits")
+enum KeyOperationType:
+  case
+  sign,
+  verify,
+  encrypt,
+  decrypt,
+  wrapKey,
+  unwrapKey,
+  deriveKey,
+  deriveBits
 end KeyOperationType
