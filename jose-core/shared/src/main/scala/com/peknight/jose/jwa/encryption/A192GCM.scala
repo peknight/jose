f4ff04d28717549467fd150aed406ea87deb8aa6
peknight <JKpeknight@gmail.com>
2024-06-07 11:35:29 +0800

    update

--- jose-core/shared/src/main/scala/com/peknight/jose/jwa/encryption/A192GCM.scala
+++ jose-core/shared/src/main/scala/com/peknight/jose/jwa/encryption/A192GCM.scala
@@ -1,9 +1,9 @@
 package com.peknight.jose.jwa.encryption
 
 import com.peknight.jose.Requirement
-import com.peknight.jose.Requirement.Recommended
+import com.peknight.jose.Requirement.Optional
 import com.peknight.security.cipher.AES_192
 
 object A192GCM extends AESGCMAlgorithm with AES_192:
-  val requirement: Requirement = Recommended
+  val requirement: Requirement = Optional
 end A192GCM
