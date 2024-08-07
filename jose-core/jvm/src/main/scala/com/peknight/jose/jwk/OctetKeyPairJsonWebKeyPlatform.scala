2ec6d00a3a8aed225364442b16176da212851c3d
peknight <JKpeknight@gmail.com>
2024-08-07 23:36:26 +0800

    update

--- jose-core/jvm/src/main/scala/com/peknight/jose/jwk/OctetKeyPairJsonWebKeyPlatform.scala
+++ jose-core/jvm/src/main/scala/com/peknight/jose/jwk/OctetKeyPairJsonWebKeyPlatform.scala
@@ -5,12 +5,13 @@ import cats.syntax.applicative.*
 import cats.syntax.either.*
 import cats.syntax.flatMap.*
 import cats.syntax.functor.*
+import cats.syntax.option.*
 import com.peknight.codec.error.DecodingFailure
 import com.peknight.jose.jwk.JsonWebKey.OctetKeyPairJsonWebKey
 import com.peknight.jose.key.OctetKeyPairOps
 import com.peknight.security.provider.Provider
 
-import java.security.PublicKey
+import java.security.{PrivateKey, PublicKey}
 
 trait OctetKeyPairJsonWebKeyPlatform { self: OctetKeyPairJsonWebKey =>
   def toPublicKey[F[_]: Sync](provider: Option[Provider]): F[Either[DecodingFailure, PublicKey]] =
@@ -20,5 +21,16 @@ trait OctetKeyPairJsonWebKeyPlatform { self: OctetKeyPairJsonWebKey =>
           .toPublicKey[F](publicKeyBytes, self.curve.asInstanceOf, provider)
           .map(_.asRight[DecodingFailure])
       case Left(error) => error.asLeft[PublicKey].pure[F]
+    }
+
+  def toPrivateKey[F[_]: Sync](provider: Option[Provider]): F[Either[DecodingFailure, Option[PrivateKey]]] =
+    self.eccPrivateKey.fold(none[PrivateKey].asRight[DecodingFailure].pure[F]) { eccPrivateKey =>
+      eccPrivateKey.decode[F].flatMap {
+        case Right(privateKeyBytes) =>
+          OctetKeyPairOps.getKeyPairOps(self.curve)
+            .toPrivateKey[F](privateKeyBytes, self.curve.asInstanceOf, provider)
+            .map(_.some.asRight[DecodingFailure])
+        case Left(error) => error.asLeft[Option[PrivateKey]].pure[F]
+      }
     }
 }
