1fce1676e4bb5259c93da09ee590853fbdc02d5a
peknight <JKpeknight@gmail.com>
2024-08-12 23:23:49 +0800

    update

--- jose-core/jvm/src/main/scala/com/peknight/jose/jws/ops/HmacSHAOps.scala
+++ jose-core/jvm/src/main/scala/com/peknight/jose/jws/ops/HmacSHAOps.scala
@@ -1,6 +1,7 @@
 package com.peknight.jose.jws.ops
 
 import cats.effect.Sync
+import cats.syntax.functor.*
 import com.peknight.jose.error.jws.{InvalidHmacSHAKeyLength, JsonWebSignatureError}
 import com.peknight.jose.jwa.signature.HmacSHAAlgorithm
 import com.peknight.security.crypto.Mac
@@ -13,6 +14,11 @@ object HmacSHAOps:
   def sign[F[_]: Sync](algorithm: HmacSHAAlgorithm, key: Key, input: ByteVector,
                        provider: Option[Provider | JProvider] = None): F[ByteVector] =
     Mac.mac[F](algorithm.mac, key, input, provider)
+
+  def verify[F[_]: Sync](algorithm: HmacSHAAlgorithm, key: Key, input: ByteVector, signature: ByteVector,
+                         provider: Option[Provider | JProvider] = None): F[Boolean] =
+    given CanEqual[ByteVector, ByteVector] = CanEqual.derived
+    sign[F](algorithm, key, input, provider).map(_ == signature)
 
   def validateKey(algorithm: HmacSHAAlgorithm, key: Key): Either[JsonWebSignatureError, Unit] =
     Option(key.getEncoded).map(_.length * 8) match
