8085ac463a017571ebb99f2fc58a666f41e43db9
peknight <JKpeknight@gmail.com>
2024-08-16 23:05:40 +0800

    update

--- jose-core/shared/src/main/scala/com/peknight/jose/jws/JsonWebSignatures.scala
+++ jose-core/shared/src/main/scala/com/peknight/jose/jws/JsonWebSignatures.scala
@@ -1,5 +1,27 @@
 package com.peknight.jose.jws
 
+import cats.Monad
+import com.peknight.codec.Encoder.encodeListA
+import com.peknight.codec.circe.iso.codec
+import com.peknight.codec.circe.sum.jsonType.given
+import com.peknight.codec.configuration.given
+import com.peknight.codec.cursor.Cursor
+import com.peknight.codec.sum.{ArrayType, NullType, ObjectType, StringType}
+import com.peknight.codec.{Codec, Decoder, Encoder}
 import com.peknight.jose.jws.Signature.Signature
+import com.peknight.jose.jws.Signature.Signature.codecSignature
+import io.circe.{Json, JsonObject}
 
 case class JsonWebSignatures(payload: String, signatures: List[Signature])
+object JsonWebSignatures:
+  given codecJsonWebSignatures[F[_], S](using
+    Monad[F], ObjectType[S], ArrayType[S], NullType[S], StringType[S],
+    Encoder[F, S, JsonObject], Decoder[F, Cursor[S], JsonObject]
+  ): Codec[F, S, Cursor[S], JsonWebSignatures] =
+    Codec.derived[F, S, JsonWebSignatures]
+
+  given jsonCodecJsonWebSignatures[F[_]: Monad]: Codec[F, Json, Cursor[Json], JsonWebSignatures] =
+    codecJsonWebSignatures[F, Json]
+
+  given circeCodecJsonWebSignatures: io.circe.Codec[JsonWebSignatures] = codec[JsonWebSignatures]
+end JsonWebSignatures
