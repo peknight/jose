98eac0c3020703ff9ce8c154ac61cf301b918d64
peknight <JKpeknight@gmail.com>
2024-09-03 00:20:08 +0800

    update

--- jose-core/jvm/src/main/scala/com/peknight/jose/jwa/encryption/AESHmacSHA2AlgorithmPlatform.scala
+++ jose-core/jvm/src/main/scala/com/peknight/jose/jwa/encryption/AESHmacSHA2AlgorithmPlatform.scala
@@ -6,10 +6,10 @@ import cats.syntax.flatMap.*
 import cats.syntax.functor.*
 import com.peknight.jose.jwx.JoseHeader
 import com.peknight.scodec.bits.ext.syntax.byteVector.{leftHalf, rightHalf}
-import com.peknight.security.cipher.{AES, Cipher}
 import com.peknight.security.cipher.mode.CBC
 import com.peknight.security.cipher.padding.PKCS5Padding
-import com.peknight.security.mac.{Hmac, MAC}
+import com.peknight.security.cipher.{AES, Cipher}
+import com.peknight.security.mac.Hmac
 import com.peknight.security.provider.Provider
 import com.peknight.security.random.SecureRandom
 import com.peknight.security.syntax.secureRandom.nextBytesF
@@ -25,18 +25,15 @@ trait AESHmacSHA2AlgorithmPlatform { self: AESHmacSHA2Algorithm =>
     ivOverride.fold(random.fold(SecureRandom[F])(_.pure[F]).flatMap(_.nextBytesF[F](ivByteLength)))(_.pure[F])
 
 
-  private def encrypt[F[_]: Sync](plaintext: ByteVector, aad: ByteVector, key: ByteVector, iv: ByteVector,
-                                  headers: JoseHeader, random: Option[JSecureRandom] = None,
+  private def encrypt[F[_]: Sync](key: ByteVector, input: ByteVector, aad: ByteVector, iv: ByteVector,
+                                  random: Option[JSecureRandom] = None,
                                   cipherProvider: Option[Provider | JProvider] = None,
-                                  macProvider: Option[Provider | JProvider]): F[ByteVector] =
-    val hmacKey = Hmac.secretKeySpec(key.leftHalf)
-    val encryptionKey = AES.secretKeySpec(key.rightHalf)
+                                  macProvider: Option[Provider | JProvider] = None): F[(ByteVector, ByteVector)] =
     for
-      cipherText <- Cipher.rawKeyEncrypt[F](javaAlgorithm, key.rightHalf, plaintext, Some(iv), random, cipherProvider)
-      // TODO
-      authenticationTag = ByteVector.empty
-      _ <- self.mac.mac[F](Hmac.secretKeySpec(key.leftHalf), authenticationTag, None, macProvider)
+      cipherText <- Cipher.rawKeyEncrypt[F](javaAlgorithm, key.rightHalf, input, Some(iv), random, cipherProvider)
+      aadLengthBytes = ByteVector.fromLong(aad.length * 8)
+      authenticationTagInput = aad ++ iv ++ cipherText ++ aadLengthBytes
+      authenticationTag <- self.mac.mac[F](Hmac.secretKeySpec(key.leftHalf), authenticationTagInput, None, macProvider)
     yield
-      cipherText
-    ???
+      (cipherText, authenticationTag.take(self.tagTruncationLength))
 }
