062bd54e8a5618dadbe7b422e10daacf94144d3f
peknight <JKpeknight@gmail.com>
2024-11-01 16:08:55 +0800

    update

--- jose-core/jvm/src/main/scala/com/peknight/jose/jwa/compression/CompressionAlgorithmPlatform.scala
+++ jose-core/jvm/src/main/scala/com/peknight/jose/jwa/compression/CompressionAlgorithmPlatform.scala
@@ -1,10 +1,11 @@
 package com.peknight.jose.jwa.compression
 
+import cats.effect.Concurrent
 import com.peknight.error.Error
 import fs2.compression.Compression
 import scodec.bits.ByteVector
 
 trait CompressionAlgorithmPlatform:
-  def compress[F[_]: Compression](data: ByteVector): F[ByteVector]
-  def decompress[F[_]: Compression](compressedData: ByteVector): F[Either[Error, ByteVector]]
+  def compress[F[_]: Concurrent: Compression](data: ByteVector): F[ByteVector]
+  def decompress[F[_]: Concurrent: Compression](compressedData: ByteVector): F[Either[Error, ByteVector]]
 end CompressionAlgorithmPlatform
