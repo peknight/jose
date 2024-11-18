b4af9667a30e72cef7c4fb81ae64987a618f4c7d
peknight <JKpeknight@gmail.com>
2024-11-18 20:55:16 +0800

    update

--- jose-core/jvm/src/test/scala/com/peknight/jose/jwk/JsonWebKeyFlatSpec.scala
+++ jose-core/jvm/src/test/scala/com/peknight/jose/jwk/JsonWebKeyFlatSpec.scala
@@ -1,73 +1,212 @@
 package com.peknight.jose.jwk
 
 import cats.Id
+import cats.data.EitherT
 import cats.effect.IO
 import cats.effect.testing.scalatest.AsyncIOSpec
-import cats.syntax.option.*
+import com.peknight.cats.ext.syntax.eitherT.eLiftET
 import com.peknight.codec.circe.parser.decode
-import com.peknight.jose.jwa.ecc.`P-384`
-import com.peknight.jose.jwk.JsonWebKey.{AsymmetricJsonWebKey, OctetSequenceJsonWebKey}
-import com.peknight.security.Security
-import com.peknight.security.bouncycastle.jce.provider.BouncyCastleProvider
-import com.peknight.security.cipher.{AES, RSA}
-import com.peknight.security.provider.Provider
-import com.peknight.security.random.SecureRandom
+import com.peknight.error.Error
+import com.peknight.error.syntax.applicativeError.asError
+import com.peknight.jose.jwa.ecc.`P-256`
+import com.peknight.jose.jwk.JsonWebKey.*
+import com.peknight.jose.jwk.KeyOperationType.*
+import com.peknight.security.cipher.RSA
+import com.peknight.security.syntax.ecParameterSpec.{privateKey, publicKey}
 import com.peknight.validation.std.either.typed
-import org.jose4j.jwk.JsonWebKey.OutputControlLevel
+import io.circe.syntax.*
 import org.scalatest.flatspec.AsyncFlatSpec
 
-import java.security.{KeyPair, SecureRandom as JSecureRandom}
+import java.security.interfaces.*
 
 class JsonWebKeyFlatSpec extends AsyncFlatSpec with AsyncIOSpec:
-  given CanEqual[JsonWebKey, JsonWebKey] = CanEqual.derived
 
-  def testKeyPair(generateKeyPair: (JSecureRandom, Provider) => IO[KeyPair]): IO[Boolean] =
+  private def isRSA(jwk: JsonWebKey): EitherT[IO, Error, Unit] =
     for
-      provider <- BouncyCastleProvider[IO]
-      _ <- Security.addProvider[IO](provider)
-      secureRandom <- SecureRandom[IO]
-      keyPair <- generateKeyPair(secureRandom, BouncyCastleProvider)
-      joseJwkEither = JsonWebKey.fromKeyPair(keyPair)
-      checkResult <- joseJwkEither match
-        case Right(joseJwk: AsymmetricJsonWebKey) => joseJwk.checkJsonWebKey[IO](Some(BouncyCastleProvider)).map(_.isRight)
-        case _ => IO(false)
-      restoredKeyPair <- joseJwkEither match
-        case Right(joseJwk: AsymmetricJsonWebKey) => joseJwk.toKeyPair[IO](Some(BouncyCastleProvider)).map(_.some)
-        case _ => IO(None)
-      jose4jJwk = org.jose4j.jwk.JsonWebKey.Factory.newJwk(keyPair.getPublic).asInstanceOf[org.jose4j.jwk.PublicJsonWebKey]
-      _ = jose4jJwk.setPrivateKey(keyPair.getPrivate)
-      jose4jJwkEither = decode[Id, JsonWebKey](jose4jJwk.toJson(OutputControlLevel.INCLUDE_PRIVATE))
+      jwk <- typed[RSAJsonWebKey](jwk).eLiftET[IO]
+      publicKey <- EitherT(jwk.toPublicKey[IO]())
+      publicKey <- typed[RSAPublicKey](publicKey).eLiftET[IO]
     yield
-      (joseJwkEither, jose4jJwkEither, restoredKeyPair, checkResult) match
-        case (Right(joseJwk), Right(jose4jJwk), Some(Right(restored)), true) =>
-          restored.getPublic.equals(keyPair.getPublic) &&
-          restored.getPrivate.equals(keyPair.getPrivate)
-        case _ => false
+      ()
 
-  "JsonWebKey" should "succeed with EC" in {
-    testKeyPair((random, provider) =>
-      `P-384`.generateKeyPair[IO](Some(random), Some(provider))
-    ).asserting(assert)
-  }
+  private def isEllipticCurve(jwk: JsonWebKey): EitherT[IO, Error, Unit] =
+    for
+      jwk <- typed[EllipticCurveJsonWebKey](jwk).eLiftET[IO]
+      publicKey <- EitherT(jwk.toPublicKey[IO]())
+      publicKey <- typed[ECPublicKey](publicKey).eLiftET[IO]
+    yield
+      ()
 
-  "JsonWebKey" should "succeed with RSA" in {
-    testKeyPair((random, provider) =>
-      RSA.keySizeGenerateKeyPair[IO](1024, Some(random), Some(provider))
-    ).asserting(assert)
-  }
-
-  "JsonWebKey" should "succeed with AES" in {
+  "JsonWebKey" should "succeed with factory with X octet key pair json web key" in {
+    val jwkJson = "{\"kty\":\"OKP\",\"d\":\"T4gjxXciGdlPcWC1Pgba0cptraIx8ZjORUyR-ttweZQ\",\"crv\":\"X25519\",\"x\":" +
+      "\"qPRE1ElE6NArtJ0rhMkjaR8_PJZLf6v6Zk_4Vo72jho\"}"
     val run =
       for
-        key <- AES.keySizeGenerateKey[IO](256)
-        joseJwkEither = JsonWebKey.fromKey(key)
-        restoredKey = joseJwkEither.flatMap(typed[OctetSequenceJsonWebKey]).map(_.toKey).toOption
-        jose4jJwk = org.jose4j.jwk.JsonWebKey.Factory.newJwk(key).asInstanceOf[org.jose4j.jwk.OctetSequenceJsonWebKey]
-        jose4jJwkEither = decode[Id, JsonWebKey](jose4jJwk.toJson(OutputControlLevel.INCLUDE_PRIVATE))
+        jwk <- decode[Id, JsonWebKey](jwkJson).eLiftET[IO]
+        jwk <- typed[OctetKeyPairJsonWebKey](jwk).eLiftET[IO]
+        publicKey <- EitherT(jwk.toPublicKey[IO]())
+        publicKey <- typed[XECPublicKey](publicKey).eLiftET[IO]
+        privateKey <- EitherT(jwk.toPrivateKey[IO]())
+        privateKey <- typed[XECPrivateKey](privateKey).eLiftET[IO]
       yield
-        (joseJwkEither, jose4jJwkEither, restoredKey) match
-          case (Right(joseJwk), Right(jose4jJwk), Some(Right(restored))) => restored.equals(key)
-          case _ => false
-    run.asserting(assert)
+        jwk.keyType == KeyType.OctetKeyPair
+    run.value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKey" should "succeed with factory with Ed octet key pair json web key" in {
+    val jwkJson = "{\"kty\":\"OKP\",\"d\":\"Y6KQHffZKlIXW1JdVvEBJCliWtuYk3pYQJoeSvfJEAw\",\"crv\":\"Ed25519\",\"x\":" +
+      "\"Jp1b9nhTp_Z2YmHC22k5oy32dIIWYOhiaD8PJQFcxgU\"}"
+    val run =
+      for
+        jwk <- decode[Id, JsonWebKey](jwkJson).eLiftET[IO]
+        jwk <- typed[OctetKeyPairJsonWebKey](jwk).eLiftET[IO]
+        publicKey <- EitherT(jwk.toPublicKey[IO]())
+        publicKey <- typed[EdECPublicKey](publicKey).eLiftET[IO]
+        privateKey <- EitherT(jwk.toPrivateKey[IO]())
+        privateKey <- typed[EdECPrivateKey](privateKey).eLiftET[IO]
+      yield
+        jwk.keyType == KeyType.OctetKeyPair
+    run.value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKey" should "succeed with factory with RSA public key" in {
+    val run =
+      for
+        publicKey <- EitherT(RSA.publicKey[IO](n, e).asError)
+        jwk <- JsonWebKey.fromKey(publicKey).eLiftET[IO]
+        _ <- isRSA(jwk)
+      yield
+        jwk.keyType == KeyType.RSA
+    run.value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKey" should "failed with factory with RSA private key" in {
+    RSA.privateKey[IO](n, d).asserting(privateKey => assert(JsonWebKey.fromKey(privateKey).isLeft))
+  }
+
+  "JsonWebKey" should "succeed with factory with EC public key" in {
+    val run =
+      for
+        publicKey <- EitherT(`P-256`.ecParameterSpec.publicKey[IO](x256, y256).asError)
+        jwk <- JsonWebKey.fromKey(publicKey).eLiftET[IO]
+        _ <- isEllipticCurve(jwk)
+      yield
+        jwk.keyType == KeyType.EllipticCurve
+    run.value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKey" should "failed with factory with EC private key" in {
+    `P-256`.ecParameterSpec.privateKey[IO](d256).asserting(privateKey => assert(JsonWebKey.fromKey(privateKey).isLeft))
+  }
+
+  "JsonWebKey" should "succeed with EC single jwk to and from json" in {
+    val jwkJson =
+      """
+        |{
+        |  "kty":"EC",
+        |  "crv":"P-256",
+        |  "x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
+        |  "y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
+        |  "use":"enc",
+        |  "kid":"1"
+        |}
+      """.stripMargin
+    val run =
+      for
+        jwk <- decode[Id, JsonWebKey](jwkJson).eLiftET[IO]
+        _ <- isEllipticCurve(jwk)
+        jsonOut = jwk.asJson.deepDropNullValues.noSpaces
+        jwk2 <- decode[Id, JsonWebKey](jsonOut).eLiftET[IO]
+        _ <- isEllipticCurve(jwk2)
+      yield
+        true
+    run.value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKey" should "succeed with RSA single jwk to and from json" in {
+    val n = "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCi" +
+      "FV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0" +
+      "zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFC" +
+      "ur-kEgU8awapJzKnqDKgw"
+    val jwkJson =
+      s"""
+        |{
+        |  "kty":"RSA",
+        |  "n": "$n",
+        |  "e":"AQAB",
+        |  "alg":"RS256"
+        |}
+      """.stripMargin
+    val run =
+      for
+        jwk <- decode[Id, JsonWebKey](jwkJson).eLiftET[IO]
+        _ <- isRSA(jwk)
+        jsonOut = jwk.asJson.deepDropNullValues.noSpaces
+        jwk2 <- decode[Id, JsonWebKey](jsonOut).eLiftET[IO]
+        _ <- isRSA(jwk2)
+      yield
+        true
+    run.value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKey" should "succeed with key ops" in {
+    val json1 = """{"kty":"oct","k":"Hdd5Uqtga_B4UilmahWJR8juxF_zw1_xaWeUGAvbg9c"}"""
+    val json2 = """{"kty":"oct","key_ops":["decrypt","encrypt"],"k":"add14qyge_v4sscm2hWJR8juxF_____cpW8U3ahcp__"}"""
+    val run =
+      for
+        jwk1 <- decode[Id, OctetSequenceJsonWebKey](json1).eLiftET[IO]
+        keyOps = List(decrypt, deriveBits, deriveKey, encrypt, sign, verify, unwrapKey, wrapKey)
+        json1 = jwk1.copy(keyOperations = Some(keyOps)).asJson.deepDropNullValues.noSpaces
+        parsedJwk1 <- decode[Id, OctetSequenceJsonWebKey](json1).eLiftET[IO]
+        jwk2 <- decode[Id, OctetSequenceJsonWebKey](json2).eLiftET[IO]
+      yield
+        jwk1.keyOperations.isEmpty && json1.contains("\"key_ops\"") && parsedJwk1.keyOperations.contains(keyOps) &&
+          jwk2.keyOperations.exists(keyOps => keyOps.length == 2 && keyOps.contains(encrypt) && keyOps.contains(decrypt))
+    run.value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKey" should "failed with handle wrong type 1" in {
+    IO.unit.asserting(_ => assert(decode[Id, JsonWebKey]("""{"kty":1}""").isLeft))
+  }
+
+  "JsonWebKey" should "failed with handle wrong type 2" in {
+    IO.unit.asserting(_ => assert(decode[Id, JsonWebKey](
+      s"""
+         |{
+         |  "kty":"RSA",
+         |  "n": 8929747471717373711113313454114,
+         |  "e":"AQAB",
+         |  "alg":"RS256"
+         |}
+      """.stripMargin
+    ).isLeft))
+  }
+
+  "JsonWebKey" should "failed with handle wrong type 3" in {
+    IO.unit.asserting(_ => assert(decode[Id, JsonWebKey](
+      s"""
+         |{ "kty":"EC",
+         |  "crv":"P-256",
+         |  "x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
+         |  "y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
+         |  "use":true,
+         |  "kid":"1"
+         |}
+      """.stripMargin
+    ).isLeft))
+  }
+
+  "JsonWebKey" should "failed with handle wrong type 4" in {
+    IO.unit.asserting(_ => assert(decode[Id, JsonWebKey](
+      s"""
+         |{
+         |  "kty":"EC",
+         |  "crv":"P-256",
+         |  "x":["MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4"],
+         |  "y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
+         |  "kid":"1s"
+         |}
+      """.stripMargin
+    ).isLeft))
   }
 end JsonWebKeyFlatSpec
--- jose-core/jvm/src/test/scala/com/peknight/jose/jwk/JsonWebKeySetFlatSpec.scala
+++ jose-core/jvm/src/test/scala/com/peknight/jose/jwk/JsonWebKeySetFlatSpec.scala
@@ -6,6 +6,7 @@ import cats.effect.IO
 import cats.effect.testing.scalatest.AsyncIOSpec
 import cats.syntax.traverse.*
 import com.peknight.cats.ext.syntax.eitherT.eLiftET
+import com.peknight.codec.base.Base64UrlNoPad
 import com.peknight.codec.circe.parser.decode
 import com.peknight.error.option.OptionEmpty
 import com.peknight.error.syntax.applicativeError.asError
@@ -14,7 +15,7 @@ import com.peknight.jose.jwa.encryption.A128KW
 import com.peknight.jose.jwa.signature.RS256
 import com.peknight.jose.jwk.JsonWebKey.{EllipticCurveJsonWebKey, OctetSequenceJsonWebKey, RSAJsonWebKey}
 import com.peknight.jose.jwk.PublicKeyUseType.{Encryption, Signature}
-import com.peknight.security.cipher.RSA
+import com.peknight.security.cipher.{AES, RSA}
 import com.peknight.security.syntax.ecParameterSpec.publicKey
 import com.peknight.validation.std.either.typed
 import io.circe.syntax.*
@@ -290,5 +291,251 @@ class JsonWebKeySetFlatSpec extends AsyncFlatSpec with AsyncIOSpec:
       .sequence
       .map(_.forall(identity))
       .value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKeySet" should "succeed with oct and default to json" in {
+    val run =
+      for
+        key1 <- EitherT(AES.keySizeGenerateKey[IO](128).asError)
+        key2 <- EitherT(AES.keySizeGenerateKey[IO](128).asError)
+        jwk1 <- JsonWebKey.fromKey(key1).eLiftET[IO]
+        jwk2 <- JsonWebKey.fromKey(key2).eLiftET[IO]
+        jwks = JsonWebKeySet(jwk1, jwk2)
+        json = jwks.asJson.deepDropNullValues.noSpaces
+        newJwks <- decode[Id, JsonWebKeySet](json).eLiftET[IO]
+      yield
+        json.contains("\"k\"") && jwks.keys.length == newJwks.keys.length
+    run.value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKeySet" should "succeed with oct keys length" in {
+    val run =
+      for
+        jwk1 <- decode[Id, OctetSequenceJsonWebKey]("""{"kty":"oct","k":"bbj4v-CvqwOm1q3WkVJEpw"}""").eLiftET[IO]
+        jwk2 <- decode[Id, OctetSequenceJsonWebKey]("""{"kty":"oct","k":"h008v_ab_Z-N7q13D-JabC"}""").eLiftET[IO]
+        jwk3 <- decode[Id, OctetSequenceJsonWebKey]("""{"kty":"oct","k":"-_-8888888888888888-_-"}""").eLiftET[IO]
+        jwk4 <- decode[Id, OctetSequenceJsonWebKey]("""{"kty":"oct","k":"__--_12_--33--_21_--__"}""").eLiftET[IO]
+        jwks = JsonWebKeySet(jwk1, jwk2, jwk3, jwk4)
+      yield
+        jwks.keys.length == 4
+    run.value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKeySet" should "succeed with parse set containing invalid" in {
+    val json =
+      """
+        |{
+        |    "keys": [
+        |        {
+        |            "kty": "EC",
+        |            "crv": "NOPERS",
+        |            "kid": "iRTU",
+        |            "x": "tcjSy7nIVZ2DVz-RKjqHIJqr5WDqZLS6fq4rEN6pfGY",
+        |            "y": "2oqx5jvggJKy-LXFjpDOpL0g_SbiLylu_8xx-dBMQeQ"
+        |        },
+        |        {
+        |            "kty": "EC",
+        |            "x": "riwTtQeRjmlDsR4PUQELhejpPkZkQstb0_Lf08qeBzM",
+        |            "y": "izN8y6z-8j8bB_Lj10gX9mnaE_E0ZK5fl0hJVyLWMKA",
+        |            "crv": "P-256"
+        |        },
+        |        {
+        |            "kty": false,
+        |            "x": "GS2tEeCRf0CFHzI_y68XiLzqa9-RpG4Xn-dq2lPtShY",
+        |            "y": "Rq6ybA7IbjhDTfvP2GSzxEql8II7RvRPb3mJ6tzZUgI",
+        |            "crv": "P-256"
+        |        },
+        |        {
+        |            "kty": "EC",
+        |            "x": "IiIIM4W-HDen_11XiGlFXh1kOxKcX1YB5gqMrCM-hMM",
+        |            "y": "57-3xqdddSBBarwwXcWu4hIG4dAlIiEYdy4aaFGb57s",
+        |            "crv": "P-256"
+        |        },
+        |        {
+        |            "kty": "EC",
+        |            "x": [
+        |                "IiIIM4W-HDen_11XiGlFXh1kOxKcX1YB5gqMrCM-hMM",
+        |                "huh"
+        |            ],
+        |            "y": "57-3xqdddSBBarwwXcWu4hIG4dAlIiEYdy4aaFGb57s",
+        |            "crv": "P-256"
+        |        },
+        |        {
+        |            "kty": "EC",
+        |            "x": "rO8MozDmEAVZ0B5zQUDD8PGosFlwmoMmi7I-1rspWz4",
+        |            "y": "I6ku1iUzFJgTnjNzjAC1sSGkYfiDqs-eEReFMLI-6n8",
+        |            "crv": "P-256"
+        |        },
+        |        {
+        |            "kty": 1,
+        |            "x": "IiIIM4W-HDen_11XiGlFXh1kOxKcX1YB5gqMrCM-hMM",
+        |            "y": "57-3xqdddSBBarwwXcWu4hIG4dAlIiEYdy4aaFGb57s",
+        |            "crv": "P-256"
+        |        },
+        |        {
+        |            "kty": 885584955514411149933357445595595145885566661,
+        |            "x": "IiIIM4W-HDen_11XiGlFXh1kOxKcX1YB5gqMrCM-hMM",
+        |            "y": "57-3xqdddSBBarwwXcWu4hIG4dAlIiEYdy4aaFGb57s",
+        |            "crv": "P-256"
+        |        },
+        |        {
+        |            "kty": {
+        |                "EC": "EC"
+        |            },
+        |            "x": "riwTtQeRjmlDsR4PUQELhejpPkZkQstb0_Lf08qeBzM",
+        |            "y": "izN8y6z-8j8bB_Lj10gX9mnaE_E0ZK5fl0hJVyLWMKA",
+        |            "crv": "P-256"
+        |        },
+        |        {
+        |            "kty": null,
+        |            "x": "riwTtQeRjmlDsR4PUQELhejpPkZkQstb0_Lf08qeBzM",
+        |            "y": "izN8y6z-8j8bB_Lj10gX9mnaE_E0ZK5fl0hJVyLWMKA",
+        |            "crv": "P-256"
+        |        }
+        |    ]
+        |}
+      """.stripMargin
+    IO.unit.asserting(_ => assert(decode[Id, JsonWebKeySet](json).map(_.keys.length == 3).getOrElse(false)))
+  }
+
+  "JsonWebKeySet" should "succeed with okps ok" in {
+    val n = "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCi" +
+      "FV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0" +
+      "zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFC" +
+      "ur-kEgU8awapJzKnqDKgw"
+    val json =
+      s"""
+        |{
+        |  "keys":
+        |    [
+        |      {
+        |        "kty":"EC",
+        |        "crv":"P-256",
+        |        "x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
+        |        "y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
+        |        "use":"enc",
+        |        "kid":"1"
+        |      },
+        |      {
+        |        "kty":"OKP",
+        |        "crv":"Ed448",
+        |        "x":"LzuoHViWG285WNae4BAFKg44npQyMRqcON7_nt9J0fJBy2zqimJTTSi0SmHqfu0GwUIXIMg7vl2A"
+        |      },
+        |      {
+        |        "kty":"OKP",
+        |        "crv":"Ed25519",
+        |        "x":"5gsMTcjHtfvEXwZuvmkEgvldWK1NHXjg6qOzC1NzsnI"
+        |      },
+        |      {
+        |        "kty":"OKP",
+        |        "crv":"X448",
+        |        "x":"ujkHCs4gobVjp7P_CfTzydh9ue3jrSh8TaIh_uNsr4kXWRGibT7OBaYxudHn5dVMfQEEPD0RwacA"
+        |      },
+        |      {
+        |        "kty":"OKP",
+        |        "crv":"X25519",
+        |        "x":"XQGokbde5czDzRvEDPFSv3jYglZN1R9upLJCfFO_-lA"
+        |      },
+        |      {
+        |        "kty":"RSA",
+        |        "n": "$n",
+        |        "e":"AQAB",
+        |        "alg":"RS256",
+        |        "kid":"2011-04-29"
+        |      }
+        |    ]
+        |}
+      """.stripMargin
+    IO.unit.asserting(_ => assert(
+      decode[Id, JsonWebKeySet](json)
+        .map(_.keys.length)
+        .map(length => 2 <= length && length <= 6)
+        .getOrElse(false)
+    ))
+  }
+
+  /*
+   * 2 tests to look at http://www.ietf.org/mail-archive/web/jose/current/msg04297.html
+   * and see/ensure that we can consume JWK input that's not strictly conforming while also
+   * producing conforming output
+   */
+
+  "JsonWebKeySet" should "succeed with salesforce" in {
+    // ~8am Aug 26, '14 from https://login.salesforce.com/id/keys
+    val jwks = "{\"keys\":[{\"kty\":\"RSA\",\"n\":\"AMCELStParLtaggkLtZh4enfxMsjpW6jAlfFjGnDsoWZ4NbG2hSWPtDyB-OisNbo" +
+      "Y2x4PeP69lBC2Hd9LxfMcFYhoQpqT7khoZMTaE-QjKCT0uiVvswaUe7Lh6gVJ2hnWehtrmGQ6cFmLP-EiQ7ls8VQa0KiDP2VYFKrrZ4kD5ozA" +
+      "F-TKs5wU5xt85u9vAZjc0u09oLc8bN4wIA7EgLtysadw-jQxhEYWgCfIzoMB75kCucRYvQHcO7L9pwh_sDPguXyyWJqRjkq0z9Ryzpavvk0Tg" +
+      "L1i_YHDRHquGq68iGLsebMoOuqx0_FNlIW9T3V7e0XkGPMAZz9gQR9UB-68zme1G6hS20FELGRQFTHH5u4CTfCVi5XEiWXQts2mNMCOavD1jf" +
+      "jfxoACuuBSmUO6QdG0UOQEMfg91OLGBOBHIEr1fH1vOj2hdVV2hzBXKJuPIGdRsxP1dubj2_tMrntHL_ZXo6yCg70YieVIslD6Ya6OAMNmXA4" +
+      "v_K_K6n4JVoXJweGxkq4uJBAW_yHcL6isEQqsZTXUZ1NaKEHlAWlUcHW6Y9t2darWIweeVn9ijgiensDMnXauGxABuiiKj5rLE-_3sb8oFMrl" +
+      "uqoZwlfoE2RMBSNAOnY7BzOYrX5MzWOOwXrgLRl47mkZ1WCBL0650o9y8e2H7wiIhqhaxust9QJ\",\"e\":\"AQAB\",\"alg\":\"RS256" +
+      "\",\"use\":\"sig\",\"kid\":\"188\"},{\"kty\":\"RSA\",\"n\":\"AKPBc9I142dEc-Srdk5sz9MVaJH_kOAM_jEIOYuTAsTTU0Im" +
+      "ae1ZMAGXjNJifpig2wsz5vcLON7_HMXoiJFWUKqwKHJ52_dDAwp1Pu6A-zLzlOEm5obi81QslWTyAUauc9DoI3MC3g-LazqKIJCzrtJMrssza" +
+      "BZK-9dpvxmdcYnPl9DJRSqt_tnCOFNpxLrofl3Mu21KgsdM0yRTzjioIRmBGWem4mdOFQvhEXFunAtfaRFpurwqmSRLCjwn2s1QKBymQLpDXd" +
+      "Fyz0Hf1usQGhp6fHu2ubRR5-nVOopISPeGYnlaeliLVrEEw8CR_g-21aVURvpVi--JYHLkHRQLZXZv_5Oxb5U13aoi63dK2Lg50xYsFErFF1g" +
+      "SW5hlbBDspWVT0AC_iuxu6dwUWOF9urzoH5bncAjo-y-1hW3dCF84k5u-MXtimRirBaaAoySNM_w-TnuW6H7MK9Qnmn4Zfe7LhuzqCJ6G7e0A" +
+      "EJ5y3AVc1D8_035Tlw3OVInj6bQNG8XXfDFRDYg20xhjc-gws3y_fOkH3CSzwfGmWt5RTdJFjwZDnJWWoC-FqciJZQenrr2doX6bfCGNv25lD" +
+      "dOpbqOjexctIUkhTFr382g_PxX93M29Sr_m9MSOlIJOHeu0TTUm03ETNjLr8fnSYDsl6q3P4RPejjqnDI6xmSaF5wgD\",\"e\":\"AQAB\"," +
+      "\"alg\":\"RS256\",\"use\":\"sig\",\"kid\":\"190\"},{\"kty\":\"RSA\",\"n\":\"AJznEDrx1fK3PoXHz_0ZsTBo8lZa7ki3h" +
+      "V06I2HG4sWgB9-rHFHo42sLN9aK1I5mKgeYrBPZ7XbC-A57HT_zAydprWA9hSIfLQZCY4F4rY3XA3Ja8BCwMfOOsASJUhEvMEenM6XSWX0sIS" +
+      "__dhBqQx-s-5ShApaoQ5W7WfshShY_QUEcGhF1le7rqtt4MVzqshDdFl5d2ST4LKHQp5V0Z_cv6-QjVfVeML81xpSYU9zb_zf2eVzWSI2Zx3Q" +
+      "rhP4rU-GtcRDRBHbOyY4OZkU5VRc2L-YkLQaO43WOaIDE4Cj5kYeoWLqi3pItwDgFH39QBmjfU2R-tFMcE8NN_g0CS-Qtkrgv7zOSiFsWcUJ4" +
+      "rm33oFAgV6SUgCWy7fM0hc7U3Ky0uPsIFB6NQPEwzWjtvPyrAVE1rK4njq9zXwp-GzzW-7fBvdFOtJVtBiIRHWt3zWJ1dFlqWVTtYwkTcvyWF" +
+      "LNxAqNBNWUCWQ-9g5ulI4rh-3kd2YDSkfbZSzXcmUqWVGTxKy61yfdHeV25iWL0V_a_d8-hkKjr-RUMtSYWrcHn8YSncoZAxB7KhCztFw6pw5" +
+      "5oMZBBFPpR2ElRs_og5VGTlGE0wrcbDw5gSFzjLsKdFMnSaYTt-qkUGg8hIxzbGCi4-Slb4wx0vBsNRYWxb7KFKwR63uIS2PT2uZnmrVf5\"," +
+      "\"e\":\"AQAB\",\"alg\":\"RS256\",\"use\":\"sig\",\"kid\":\"192\"}]}"
+    val run =
+      for
+        jsonWebKeySet <- decode[Id, JsonWebKeySet](jwks).eLiftET[IO]
+        jwk188 <- jsonWebKeySet.keys
+          .find(jwk => jwk.keyID.contains(KeyId("188")) && jwk.publicKeyUse.contains(Signature))
+          .toRight(OptionEmpty)
+          .eLiftET[IO]
+        jwk188 <- typed[RSAJsonWebKey](jwk188).eLiftET[IO]
+        publicKey <- EitherT(jwk188.toPublicKey[IO]())
+        jwk188 <- JsonWebKey.fromKey(publicKey).eLiftET[IO]
+        jwk188 <- typed[RSAJsonWebKey](jwk188).eLiftET[IO]
+        modulus188 <- jwk188.modulus.decode[Id].eLiftET[IO]
+      yield
+        jsonWebKeySet.keys.length == 3 && modulus188.headOption.exists(_ != 0)
+    run.value.asserting(value => assert(value.getOrElse(false)))
+  }
+
+  "JsonWebKeySet" should "succeed with google" in {
+    // ~8am Aug 26, '14 from https://www.googleapis.com/oauth2/v2/certs
+    val n1 = "vl9eiLnGMX7r0f7i7sSqCN5zpISYRtqrZA8JfcVSq3FrqZFoUNcMCDbSaWGzWWCTkvN3jQEkgYpCpwRAOMYM08IXm46UwxMWlcb8c4" +
+      "7LGbdFWzyf3t_3FcqASMp6BuEnCCciifAcDeiqG4JYmkux-KUSWYjXGFOxgjL0xZ4M3O8="
+    val n2 = "x_s89G0aZsHdL81sgDN8-zPi9oq-5rlP5j850QllJUMD4PBEEo9KnfoKC9WaSJ2_oOI3W8KOLk4i993J4IGzJFlrNKt2xNSL60iQ9n" +
+      "DwGMhIXnieGyXosKRXhepaySCBQysuW8OiVlDVEoFS2VHvC_6bt5QZaitl7AYZxPoPujk="
+    val jwks =
+      s"""
+         |{
+         |  "keys": [
+         |    {
+         |      "kty": "RSA",
+         |      "alg": "RS256",
+         |      "use": "sig",
+         |      "kid": "ce808d4fb2eabff22a608e0c7a14300cc04f2606",
+         |      "n": "$n1",
+         |      "e": "AQAB"
+         |    },
+         |    {
+         |      "kty": "RSA",
+         |      "alg": "RS256",
+         |      "use": "sig",
+         |      "kid": "ce3dde4df07fe0794fcff86642b4b11f8026f43f",
+         |      "n": "$n2",
+         |      "e": "AQAB"
+         |    }
+         |  ]
+         |}
+      """.stripMargin
+    val run =
+      for
+        jsonWebKeySet <- decode[Id, JsonWebKeySet](jwks)
+        json = jsonWebKeySet.asJson.deepDropNullValues.noSpaces
+      yield
+        jsonWebKeySet.keys.length == 2 && !json.contains("=")
+    IO.unit.asserting(_ => assert(run.getOrElse(false)))
   }
 end JsonWebKeySetFlatSpec
