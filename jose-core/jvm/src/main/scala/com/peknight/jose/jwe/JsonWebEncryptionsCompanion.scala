2f8384da3e6be24e2145f65475fd1ee1cf29719e
peknight <JKpeknight@gmail.com>
2024-12-06 19:15:08 +0800

    update

--- jose-core/jvm/src/main/scala/com/peknight/jose/jwe/JsonWebEncryptionsCompanion.scala
+++ jose-core/jvm/src/main/scala/com/peknight/jose/jwe/JsonWebEncryptionsCompanion.scala
@@ -1,3 +1,18 @@
 package com.peknight.jose.jwe
 
-trait JsonWebEncryptionsCompanion
+import cats.data.NonEmptyList
+import cats.effect.Async
+import com.peknight.jose.jwx.{JoseConfiguration, JoseHeader}
+import com.peknight.error.Error
+import fs2.compression.Compression
+import scodec.bits.ByteVector
+
+trait JsonWebEncryptionsCompanion:
+  def encrypt[F[_]: Async: Compression](primitives: NonEmptyList[EncryptionPrimitive], header: JoseHeader,
+                                        plaintext: ByteVector, cekOverride: Option[ByteVector] = None,
+                                        ivOverride: Option[ByteVector] = None, aadOverride: Option[ByteVector] = None,
+                                        sharedHeader: Option[JoseHeader] = None,
+                                        configuration: JoseConfiguration = JoseConfiguration.default)
+  : F[Either[Error, JsonWebEncryptions]] =
+    ???
+end JsonWebEncryptionsCompanion
