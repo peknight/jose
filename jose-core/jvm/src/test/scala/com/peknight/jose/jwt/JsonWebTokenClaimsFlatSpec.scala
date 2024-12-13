245b033e25e6444f159c03c8c076a19ed0db76c0
peknight <JKpeknight@gmail.com>
2024-12-13 19:59:14 +0800

    update

--- jose-core/jvm/src/test/scala/com/peknight/jose/jwt/JsonWebTokenClaimsFlatSpec.scala
+++ jose-core/jvm/src/test/scala/com/peknight/jose/jwt/JsonWebTokenClaimsFlatSpec.scala
@@ -34,4 +34,33 @@ class JsonWebTokenClaimsFlatSpec extends AnyFlatSpec:
         audience.contains("one") && audience.contains("two") && audience.contains("three"))
     ))
   }
+  "JsonWebTokenClaims" should "succeed with get audience array" in {
+    assert(decode[Id, JsonWebTokenClaims]("""{"aud":[]}""").exists(claims => claims.audience.exists(_.isEmpty)))
+  }
+  "JsonWebTokenClaims" should "failed with bad audience 1" in {
+    assert(decode[Id, JsonWebTokenClaims]("""{"aud":1996}""").isLeft)
+  }
+  "JsonWebTokenClaims" should "failed with bad audience 2" in {
+    assert(decode[Id, JsonWebTokenClaims]("""{"aud":["value", "other", 2, "value"]}""").isLeft)
+  }
+  "JsonWebTokenClaims" should "succeed with get null subject" in {
+    assert(decode[Id, JsonWebTokenClaims]("""{"exp":123456781}""").exists(_.subject.isEmpty))
+  }
+  "JsonWebTokenClaims" should "succeed with get subject" in {
+    val subject = "subject@example.com"
+    assert(decode[Id, JsonWebTokenClaims](s"""{"sub":"$subject"}""").exists(_.subject.contains(subject)))
+  }
+  "JsonWebTokenClaims" should "failed with bad subject" in {
+    assert(decode[Id, JsonWebTokenClaims]("""{"sub":["nope", "not", "good"]}""").isLeft)
+  }
+  "JsonWebTokenClaims" should "succeed with get null jwt id" in {
+    assert(decode[Id, JsonWebTokenClaims]("""{"whatever":123456781}""").exists(_.jwtID.isEmpty))
+  }
+  "JsonWebTokenClaims" should "succeed with get jwt id" in {
+    val jwtId = "Xk9c2inNN8fFs60epZil3"
+    assert(decode[Id, JsonWebTokenClaims](s"""{"jti":"$jwtId"}""").exists(_.jwtID.contains(jwtId)))
+  }
+  "JsonWebTokenClaims" should "failed with bad jwt id" in {
+    assert(decode[Id, JsonWebTokenClaims]("""{"jti":["nope", "not", "good"]}""").isLeft)
+  }
 end JsonWebTokenClaimsFlatSpec
